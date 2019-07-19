94b72c58a6d2b1d7c58416fb9ee9280882c1570d
zhuangsw <1246419846@qq.com>
2019-07-19 16:28:08 +0800

    7-19 下载页面 demo页面整合一起

--- demo/js/ajax.js
+++ demo/js/ajax.js
@@ -1,66 +1,21 @@
-//url接口地址
-var commonUrl="http://127.0.0.1:11200";
-
-//页面加载所要进行的操作
-$(function () {
-    //设置ajax当前状态(是否可以发送);
-    ajaxStatus = true;
-});
-
 // ajax封装
-function ajax(url, data, dataType) {
+function ajax(url, data,callback) {
     var type =  'post';//请求类型
     var dataType =  'json';//接收数据类型
     var async =  true;//异步请求
     var alone =  false;//独立提交（一次有效的提交）
     var cache = false;//浏览器历史缓存
+    var result;
     var success =  function (res) {
-
-            return res;
-            /*console.log('请求成功');*/
-            //setTimeout(function () {
-            //    console.log(data.msg);//
-            //},500);
-            //if(data.status){//服务器处理成功
-            //    setTimeout(function () {
-            //        if(data.url){
-            //            location.replace(data.url);
-            //        }else{
-            //            location.reload(true);
-            //        }
-            //    },1500);
-            //}else{//服务器处理失败
-            //    if(alone){//改变ajax提交状态
-            //        ajaxStatus = true;
-            //    }
-            //}
-        };
-    var error = function (data) {
-            /*console.error('请求成功失败');*/
-            /*data.status;//错误状态吗*/
-            //layer.closeAll('loading');
-            setTimeout(function () {
-                if(data.status == 404){
-                    console.log('请求失败，请求未找到');
-                }else if(data.status == 503){
-                    console.log('请求失败，服务器内部错误');
-                }else {
-                    console.log('请求失败,网络连接超时');
-                }
-                ajaxStatus = true;
-            },500);
-        };
-    /*判断是否可以发送请求*/
-    if(!ajaxStatus){
-        return false;
-    }
-    ajaxStatus = false;//禁用ajax请求
-    /*正常情况下1秒后可以再次多个异步请求，为true时只可以有一次有效请求（例如添加数据）*/
-    if(!alone){
-        setTimeout(function () {
-            ajaxStatus = true;
-        },1000);
-    }
+        console.log(res);
+        callback(res);
+        //result=res;
+    };
+    var error = function (res) {
+        //result=res.responseText;
+        callback(res.responseText);
+        console.log(res.responseText);
+    };
     $.ajax({
         'url': url,
         'data': data,
@@ -70,35 +25,5 @@ function ajax(url, data, dataType) {
         'success': success,
         'error': error,
     });
-}
-
-// submitAjax(post方式提交)
-function submitAjax(form, success, cache, alone) {
-    cache = cache || true;
-    var form = $(form);
-    var url = form.attr('action');
-    var data = form.serialize();
-    ajax(url, data, success, cache, alone, false, 'post','json');
-}
-/*//调用实例
- $(function () {
- $('#form-login').submit(function () {
- submitAjax('#form-login');
- return false;
- });
- });*/
-
-// ajax提交(post方式提交)
-function post(url, data, success, cache, alone) {
-    ajax(url, data, success, cache, alone, false, 'post','json');
-}
-
-// ajax提交(get方式提交)
-function get(url, success, cache, alone) {
-    ajax(url, {}, success, alone, false, 'get','json');
-}
-
-// jsonp跨域请求(get方式提交)
-function jsonp(url, success, cache, alone) {
-    ajax(url, {}, success, cache, alone, false, 'get','jsonp');
+    //return result
 }
